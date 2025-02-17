2b4a5876489ceb93fb0229c80ec7764c127e2561
fs4nt0s94 <fellipesdc.01@gmail.com>
2025-02-17 16:49:13 +0000

    Adicionando Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,34 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+
+    char estado;
+    char codigocarta [20];
+    char nomedacidade [50];
+    int populacao, pontosturisticos;
+    float areakm, pib;
+
+    printf("Digite o Estado:\n");
+    scanf("%d", &estado);
+
+    printf("Digite o Codigo da Carta:\n");
+    scanf("%s", &codigocarta);
+
+    printf("Digite o nome da Cidade\n");
+    scanf("%s", &nomedacidade);
+
+    printf("Digite a População da Cidade\n");
+    scanf("%d", &populacao);
+
+    printf("Digite Quantos Pontos Turisticos tem na cidade");
+    scanf("%d", &pontosturisticos);
+
+    printf("Digite a Area da cidade em KM");
+    scanf("%f", &areakm);
+
+    printf("Digite o PIB da cidade");
+    scanf("%f", &pib);
+    
+
     return 0;
 }
